ecdea450a1a4d85c85c8b218d17fea944b31e7b4
1999onell <1999onell@gmail.com>
2021-03-20 11:13:49 +0900

    파일목록 읽어와서, html으로 출력하는것 까지 함

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -1,13 +1,28 @@
 "use strict";
 
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index.ejs", {list: "<li>this is list</li>"});
+    res.render("index.ejs", {list: getList()});
 });
 
 router.post("/create-process", (req, res) => {
     res.send("create하는중");
-})
+});
+
+// html list 코드를 생성하자
+function getList() {
+    var files = fs.readdirSync('./database');
+
+    let list = '';
+    let i;
+    for (i = 0; i < files.length; i++) {
+        list += `<li>${files[i]}</li>`;
+    }
+    return list;
+}
+
+
 module.exports = router;
