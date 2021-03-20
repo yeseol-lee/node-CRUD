a2f53a478611f6dc974f5f09867e3a6b90641ed5
1999onell <1999onell@gmail.com>
2021-03-20 12:22:37 +0900

    제목, 내용 칸을 만듦. list클릭하면 쿼리스트링 변하고, 그에따라 제목.내용 변하게 해야함

--- app/src/public/js/index.js
+++ app/src/public/js/index.js
@@ -1,8 +1,22 @@
+"use strict";
+
 const createBtn = document.querySelector("#create");
 
+// create버튼 클릭 시 createbox 나옴, 다시 클릭 하면 없어짐
 createBtn.addEventListener("click", () => {
     const style = document.querySelector("#create-box").style;
     
     style.display === "block" ? style.display = "none" : style.display = "block";
 });
+
+//글목록 list클릭 시 발생하는 이벤트
+const listArr = document.getElementsByClassName("list");
+console.log(listArr);
+
+let i;
+for (i = 0; i <listArr.length; i++) {
+    listArr[i].addEventListener("click", () => {
+        alert("hello");
+    })
+}
 
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,7 +5,11 @@ const fs = require("fs");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index.ejs", {list: getList()});
+    res.render("index.ejs", {
+        list: getList(),
+        title: getTitle(),
+        contents: getContents()
+    });
 });
 
 router.post("/create-process", (req, res) => {
@@ -14,14 +18,22 @@ router.post("/create-process", (req, res) => {
 
 // html list 코드를 생성하자
 function getList() {
-    var files = fs.readdirSync('./database');
+    var files = fs.readdirSync('./database/articles');
 
     let list = '';
     let i;
     for (i = 0; i < files.length; i++) {
-        list += `<li>${files[i]}</li>`;
+        list += `<li class="list">${files[i]}</li>`;
     }
     return list;
+}
+
+function getTitle() {
+    return "나는 제목"
+}
+
+function getContents() {
+    return "나는 내용"
 }
 
 
